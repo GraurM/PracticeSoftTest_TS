34b268e929f5e29a0dfc81c5b6ba181abf0f65d6
Mike G <mihai.graur@amdaris.com>
2026-03-03 14:57:07 +0200

    final thoughts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 import { getTestConfig } from './src/config';
 
 const config = getTestConfig();
+const isCI = !!process.env.CI;
 
 /**
  * Read environment variables from file.
@@ -20,9 +21,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: isCI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: isCI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   expect: {
@@ -30,9 +31,9 @@ export default defineConfig({
       animations: 'disabled',
       caret: 'hide',
       scale: 'css',
-      threshold: 0.2,
-      maxDiffPixelRatio: 0.003,
-      maxDiffPixels: 120,
+      threshold: isCI ? 0.3 : 0.2,
+      maxDiffPixelRatio: isCI ? 0.015 : 0.003,
+      maxDiffPixels: isCI ? 2500 : 120,
     },
   },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
